e7bdd298959559d9b101a267e7c4364d03a949a9
bobijackrussel <andjela.milasinovic@student.flf.unibl.org>
2026-01-13 10:23:30 +0100

    Updated DocumentsView.xaml

---
+++ wpf/Views/Controls/FilterControl.xaml.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DocumentManagement.Views.Controls
+{
+    public partial class FilterControl : UserControl
+    {
+        public FilterControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
